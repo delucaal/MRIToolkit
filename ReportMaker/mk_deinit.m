29b7b96c3cdb6b22e8170b0618379f3cfc12e1c6
Alberto De Luca <43113986+delucaal@users.noreply.github.com>
2020-03-06 14:44:17 +0100

    ReportMaker integration

---
+++ ReportMaker/mk_deinit.m
@@ -0,0 +1,9 @@
+%%%$ Included in MRIToolkit (https://github.com/delucaal/MRIToolkit) %%%%%% Alberto De Luca - alberto@isi.uu.nl $%%%%%% Distributed under the terms of LGPLv3  %%%
+
+
+
+% A. De Luca
+% Sub-toolbox specific initialization
+% 06/03/2020: creation - v1.0
+
+rmpath(get_executed_file_path())
---
+++ ReportMaker/mk_init.m
@@ -0,0 +1,11 @@
+%%%$ Included in MRIToolkit (https://github.com/delucaal/MRIToolkit) %%%%%% Alberto De Luca - alberto@isi.uu.nl $%%%%%% Distributed under the terms of LGPLv3  %%%
+
+
+
+% A. De Luca
+% Sub-toolbox specific initialization
+% 06/03/2020: creation - v1.0
+global MRIToolkit;
+MRIToolkit.reportmaker_version = 1.0;
+
+addpath(get_executed_file_path())
